1b095ba40b9bcbfbd6bf20fd41420064ac8266fd
Yi Ai <aiyi.hack@gmail.com>
2026-03-21 20:57:09 +1100

    fix deployment error

--- lib/routes/twitter/api/mobile-api/login.ts
+++ lib/routes/twitter/api/mobile-api/login.ts
@@ -1,31 +1,32 @@
 // https://github.com/BANKA2017/twitter-monitor/blob/node/apps/open_account/scripts/login.mjs
 
-import crypto from 'node:crypto';
+import crypto from "node:crypto";
 
-import { authenticator } from 'otplib';
-import { RateLimiterMemory, RateLimiterQueue, RateLimiterRedis } from 'rate-limiter-flexible';
-import { v5 as uuidv5 } from 'uuid';
+import { generateSync, NobleCryptoPlugin, ScureBase32Plugin } from "otplib";
+import { RateLimiterMemory, RateLimiterQueue, RateLimiterRedis } from "rate-limiter-flexible";
+import { v5 as uuidv5 } from "uuid";
 
-import cache from '@/utils/cache';
-import got from '@/utils/got';
-import logger from '@/utils/logger';
-import ofetch from '@/utils/ofetch';
+import cache from "@/utils/cache";
+import got from "@/utils/got";
+import logger from "@/utils/logger";
+import ofetch from "@/utils/ofetch";
 
-import { bearerToken, guestActivateUrl } from './constants';
+import { bearerToken, guestActivateUrl } from "./constants";
 
-const ENDPOINT = 'https://api.x.com/1.1/onboarding/task.json';
+const ENDPOINT = "https://api.x.com/1.1/onboarding/task.json";
 
-const NAMESPACE = 'd41d092b-b007-48f7-9129-e9538d2d8fe9';
+const NAMESPACE = "d41d092b-b007-48f7-9129-e9538d2d8fe9";
 
 const headers = {
-    'User-Agent': 'TwitterAndroid/10.21.0-release.0 (310210000-r-0) ONEPLUS+A3010/9 (OnePlus;ONEPLUS+A3010;OnePlus;OnePlus3;0;;1;2016)',
-    'X-Twitter-API-Version': '5',
-    'X-Twitter-Client': 'TwitterAndroid',
-    'X-Twitter-Client-Version': '10.21.0-release.0',
-    'OS-Version': '28',
-    'System-User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 9; ONEPLUS A3010 Build/PKQ1.181203.001)',
-    'X-Twitter-Active-User': 'yes',
-    'Content-Type': 'application/json',
+    "User-Agent":
+        "TwitterAndroid/10.21.0-release.0 (310210000-r-0) ONEPLUS+A3010/9 (OnePlus;ONEPLUS+A3010;OnePlus;OnePlus3;0;;1;2016)",
+    "X-Twitter-API-Version": "5",
+    "X-Twitter-Client": "TwitterAndroid",
+    "X-Twitter-Client-Version": "10.21.0-release.0",
+    "OS-Version": "28",
+    "System-User-Agent": "Dalvik/2.1.0 (Linux; U; Android 9; ONEPLUS A3010 Build/PKQ1.181203.001)",
+    "X-Twitter-Active-User": "yes",
+    "Content-Type": "application/json",
     Authorization: bearerToken,
 };
 
@@ -44,7 +45,11 @@ const loginLimiter = cache.clients.redisClient
 
 const loginLimiterQueue = new RateLimiterQueue(loginLimiter);
 
-const postTask = async (flowToken: string, subtaskId: string, subtaskInput: Record<string, unknown>) =>
+const postTask = async (
+    flowToken: string,
+    subtaskId: string,
+    subtaskInput: Record<string, unknown>,
+) =>
     await got.post(ENDPOINT, {
         headers,
         json: {
@@ -58,45 +63,49 @@ const postTask = async (flowToken: string, subtaskId: string, subtaskInput: Reco
 // If there are missing tasks in the future, simply add the implementation of that task to it.
 const flowTasks = {
     async LoginEnterUserIdentifier({ flowToken, username }) {
-        return await postTask(flowToken, 'LoginEnterUserIdentifier', {
+        return await postTask(flowToken, "LoginEnterUserIdentifier", {
             enter_text: {
                 suggestion_id: null,
                 text: username,
-                link: 'next_link',
+                link: "next_link",
             },
         });
     },
     async LoginEnterPassword({ flowToken, password }) {
-        return await postTask(flowToken, 'LoginEnterPassword', {
+        return await postTask(flowToken, "LoginEnterPassword", {
             enter_password: {
                 password,
-                link: 'next_link',
+                link: "next_link",
             },
         });
     },
     async LoginEnterAlternateIdentifierSubtask({ flowToken, phoneOrEmail }) {
-        return await postTask(flowToken, 'LoginEnterAlternateIdentifierSubtask', {
+        return await postTask(flowToken, "LoginEnterAlternateIdentifierSubtask", {
             enter_text: {
                 suggestion_id: null,
                 text: phoneOrEmail,
-                link: 'next_link',
+                link: "next_link",
             },
         });
     },
     async AccountDuplicationCheck({ flowToken }) {
-        return await postTask(flowToken, 'AccountDuplicationCheck', {
+        return await postTask(flowToken, "AccountDuplicationCheck", {
             check_logged_in_account: {
-                link: 'AccountDuplicationCheck_false',
+                link: "AccountDuplicationCheck_false",
             },
         });
     },
     async LoginTwoFactorAuthChallenge({ flowToken, authenticationSecret }) {
-        const token = authenticator.generate(authenticationSecret);
-        return await postTask(flowToken, 'LoginTwoFactorAuthChallenge', {
+        const token = generateSync({
+            secret: authenticationSecret,
+            crypto: new NobleCryptoPlugin(),
+            base32: new ScureBase32Plugin(),
+        });
+        return await postTask(flowToken, "LoginTwoFactorAuthChallenge", {
             enter_text: {
                 suggestion_id: null,
                 text: token,
-                link: 'next_link',
+                link: "next_link",
             },
         });
     },
@@ -109,35 +118,35 @@ async function login({ username, password, authenticationSecret, phoneOrEmail })
             try {
                 await loginLimiterQueue.removeTokens(1);
 
-                logger.debug('Twitter login start.');
+                logger.debug("Twitter login start.");
 
-                headers['X-Twitter-Client-DeviceID'] = uuidv5(username, NAMESPACE);
+                headers["X-Twitter-Client-DeviceID"] = uuidv5(username, NAMESPACE);
 
-                const ct0 = crypto.randomUUID().replaceAll('-', '');
+                const ct0 = crypto.randomUUID().replaceAll("-", "");
                 const guestToken = await got(guestActivateUrl, {
                     headers: {
                         authorization: bearerToken,
-                        'x-csrf-token': ct0,
-                        cookie: 'ct0=' + ct0,
+                        "x-csrf-token": ct0,
+                        cookie: "ct0=" + ct0,
                     },
-                    method: 'POST',
+                    method: "POST",
                 });
-                logger.debug('Twitter login: guest token');
+                logger.debug("Twitter login: guest token");
 
-                headers['x-guest-token'] = guestToken.data.guest_token;
+                headers["x-guest-token"] = guestToken.data.guest_token;
 
                 let task = await ofetch
                     .raw(
                         ENDPOINT +
-                            '?' +
+                            "?" +
                             new URLSearchParams({
-                                flow_name: 'login',
-                                api_version: '1',
-                                known_device_token: '',
-                                sim_country_code: 'us',
+                                flow_name: "login",
+                                api_version: "1",
+                                known_device_token: "",
+                                sim_country_code: "us",
                             }).toString(),
                         {
-                            method: 'POST',
+                            method: "POST",
                             headers,
                             body: {
                                 flow_token: null,
@@ -145,25 +154,26 @@ async function login({ username, password, authenticationSecret, phoneOrEmail })
                                     country_code: null,
                                     flow_context: {
                                         referrer_context: {
-                                            referral_details: 'utm_source=google-play&utm_medium=organic',
-                                            referrer_url: '',
+                                            referral_details:
+                                                "utm_source=google-play&utm_medium=organic",
+                                            referrer_url: "",
                                         },
                                         start_location: {
-                                            location: 'deeplink',
+                                            location: "deeplink",
                                         },
                                     },
                                     requested_variant: null,
                                     target_user_id: 0,
                                 },
                             },
-                        }
+                        },
                     )
                     .then(({ headers: _headers, _data }) => {
-                        headers.att = _headers.get('att');
+                        headers.att = _headers.get("att");
                         return { data: _data };
                     });
 
-                logger.debug('Twitter login flow start.');
+                logger.debug("Twitter login flow start.");
                 const runTask = async ({ data }) => {
                     const { subtask_id, open_account } = data.subtasks.shift();
 
@@ -174,7 +184,9 @@ async function login({ username, password, authenticationSecret, phoneOrEmail })
 
                     // If task does not exist in `flowTasks`, we need to implement it.
                     if (!(subtask_id in flowTasks)) {
-                        logger.error(`Twitter login flow task failed: unknown subtask: ${subtask_id}`);
+                        logger.error(
+                            `Twitter login flow task failed: unknown subtask: ${subtask_id}`,
+                        );
                         return;
                     }
 
@@ -190,12 +202,14 @@ async function login({ username, password, authenticationSecret, phoneOrEmail })
                     return await runTask(task);
                 };
                 const authentication = await runTask(task);
-                logger.debug('Twitter login flow finished.');
+                logger.debug("Twitter login flow finished.");
 
                 if (authentication) {
-                    logger.debug('Twitter login success.', authentication);
+                    logger.debug("Twitter login success.", authentication);
                 } else {
-                    logger.error(`Twitter login failed. ${JSON.stringify(task.data?.subtasks, null, 2)}`);
+                    logger.error(
+                        `Twitter login failed. ${JSON.stringify(task.data?.subtasks, null, 2)}`,
+                    );
                 }
 
                 return authentication;
@@ -204,7 +218,7 @@ async function login({ username, password, authenticationSecret, phoneOrEmail })
             }
         },
         60 * 60 * 24 * 30, // 30 days
-        false
+        false,
     )) as {
         oauth_token: string;
         oauth_token_secret: string;
--- lib/routes/twitter/api/web-api/login.ts
+++ lib/routes/twitter/api/web-api/login.ts
@@ -1,10 +1,10 @@
-import { authenticator } from 'otplib';
-import { RateLimiterMemory, RateLimiterQueue, RateLimiterRedis } from 'rate-limiter-flexible';
-import { CookieJar } from 'tough-cookie';
+import { generateSync, NobleCryptoPlugin, ScureBase32Plugin } from "otplib";
+import { RateLimiterMemory, RateLimiterQueue, RateLimiterRedis } from "rate-limiter-flexible";
+import { CookieJar } from "tough-cookie";
 
-import cache from '@/utils/cache';
-import logger from '@/utils/logger';
-import puppeteer from '@/utils/puppeteer';
+import cache from "@/utils/cache";
+import logger from "@/utils/logger";
+import puppeteer from "@/utils/puppeteer";
 
 const loginLimiter = cache.clients.redisClient
     ? new RateLimiterRedis({
@@ -31,32 +31,40 @@ async function login({ username, password, authenticationSecret }) {
         const cookieJar = new CookieJar();
         const browser = await puppeteer();
         const page = await browser.newPage();
-        await page.goto('https://x.com/i/flow/login');
+        await page.goto("https://x.com/i/flow/login");
         await page.waitForSelector('input[autocomplete="username"]');
         await page.type('input[autocomplete="username"]', username);
-        const buttons = await page.$$('button');
+        const buttons = await page.$$("button");
         await buttons[3]?.click();
         await page.waitForSelector('input[autocomplete="current-password"]');
         await page.type('input[autocomplete="current-password"]', password);
         (await page.waitForSelector('button[data-testid="LoginForm_Login_Button"]'))?.click();
         if (authenticationSecret) {
             await page.waitForSelector('input[inputmode="numeric"]');
-            const token = authenticator.generate(authenticationSecret);
+            const token = generateSync({
+                secret: authenticationSecret,
+                crypto: new NobleCryptoPlugin(),
+                base32: new ScureBase32Plugin(),
+            });
             await page.type('input[inputmode="numeric"]', token);
             (await page.waitForSelector('button[data-testid="ocfEnterTextNextButton"]'))?.click();
         }
         const waitForRequest = new Promise<string>((resolve) => {
-            page.on('response', async (response) => {
-                if (response.url().includes('/HomeTimeline')) {
+            page.on("response", async (response) => {
+                if (response.url().includes("/HomeTimeline")) {
                     const data = await response.json();
-                    const message = data?.data?.home?.home_timeline_urt?.instructions?.[0]?.entries?.[0]?.entryId;
-                    if (message === 'messageprompt-suspended-prompt') {
-                        logger.error(`twitter debug: twitter username ${username} login failed: messageprompt-suspended-prompt`);
-                        resolve('');
+                    const message =
+                        data?.data?.home?.home_timeline_urt?.instructions?.[0]?.entries?.[0]
+                            ?.entryId;
+                    if (message === "messageprompt-suspended-prompt") {
+                        logger.error(
+                            `twitter debug: twitter username ${username} login failed: messageprompt-suspended-prompt`,
+                        );
+                        resolve("");
                     }
                     const cookies = await page.cookies();
                     for (const cookie of cookies) {
-                        cookieJar.setCookieSync(`${cookie.name}=${cookie.value}`, 'https://x.com');
+                        cookieJar.setCookieSync(`${cookie.name}=${cookie.value}`, "https://x.com");
                     }
                     logger.debug(`twitter debug: twitter username ${username} login success`);
                     resolve(JSON.stringify(cookieJar.serializeSync()));
